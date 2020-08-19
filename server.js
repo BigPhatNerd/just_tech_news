2ea4df5c22fca1887b84c2320d9a6390675405be
Wilson <wilsonhorrell@git.training>
2020-08-19 12:17:12 -0500

    Added jest testing functionality

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const helpers = require('./utils/helpers');
 
 const sess = {
     secret: "superdupertopsecret",
